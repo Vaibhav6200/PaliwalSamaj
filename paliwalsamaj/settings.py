503391c2f38303d368f31dee757da4eadc2e4ae0
Vaibhav Paliwal <vaibhavpaliwal620@gmail.com>
2025-06-15 19:41:43 +0530

    Jazzmin settings modified

--- paliwalsamaj/settings.py
+++ paliwalsamaj/settings.py
@@ -164,10 +164,10 @@ JAZZMIN_SETTINGS = {
     "site_brand": "Paliwal Samaj",
 
     # Logo to use for your site, must be present in static files, used for brand on top left
-    "site_logo": "images/ps-light-logo.png",
+    "site_logo": "images/ps-logo.png",
 
     # Logo to use for your site, must be present in static files, used for login form logo (defaults to site_logo)
-    "login_logo": "images/ps-light-logo.png",
+    "login_logo": "images/ps-logo.png",
 
     # Logo to use for login form in dark themes (defaults to login_logo)
     "login_logo_dark": "images/ps-light-logo.png",
