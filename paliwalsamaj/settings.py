cb351489cb33f3f8e3f33364d82ea20f70144ac2
Vaibhav Paliwal <vaibhavpaliwal620@gmail.com>
2025-06-15 19:02:14 +0530

    testing github

--- paliwalsamaj/settings.py
+++ paliwalsamaj/settings.py
@@ -30,6 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+# Added a coment on allowed hosts
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS')
 
 
