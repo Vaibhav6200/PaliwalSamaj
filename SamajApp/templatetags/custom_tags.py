3323b7139398f3210f34dec8d41a782e2614b21d
Vaibhav Paliwal <vaibhavpaliwal620@gmail.com>
2025-06-16 11:48:55 +0530

    Modified names in index

---
+++ SamajApp/templatetags/custom_tags.py
@@ -0,0 +1,21 @@
+from django import template
+from datetime import date
+
+
+register = template.Library()
+
+
+@register.filter(name='get_member_age')
+def get_member_age(dob):
+    if not dob:
+        return ''
+    today = date.today()
+    try:
+        birthday = dob.replace(year=today.year)
+        # If birthday hasn't occurred yet this year
+        if birthday > today:
+            return today.year - dob.year - 1
+        else:
+            return today.year - dob.year
+    except AttributeError:
+        return ''
