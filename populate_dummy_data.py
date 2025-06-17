248ef3c3f068c40743a52a05bbb9cbb809883ef1
Vaibhav Paliwal <vaibhavpaliwal620@gmail.com>
2025-06-17 19:04:22 +0530

    converted village fake to city

--- populate_dummy_data.py
+++ populate_dummy_data.py
@@ -74,7 +74,7 @@ def populate_members(n):
             whatsapp_number=f"98765432{i:02}",
             gotra=random.choice(gotras)[0],
             current_address=random.choice(locations),
-            current_address_village = fake.village(),
+            current_address_village = fake.city(),
             current_address_city = fake.city(),
             current_address_state = fake.state(),
             qualification_type=random.choice(['school', 'undergraduate', 'graduate']),
