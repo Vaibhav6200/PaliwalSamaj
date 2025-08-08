440f6ed5213c110f52fc37ee9988e7d7f14b8063
Vaibhav Paliwal <vaibhavpaliwal620@gmail.com>
2025-08-08 21:10:05 +0530

    added alignment

--- paliwalsamaj/ckeditor_config.py
+++ paliwalsamaj/ckeditor_config.py
@@ -54,7 +54,7 @@ CKEDITOR_5_CONFIGS = {
                     'subscript', 'superscript', 'highlight', '|', 'sourceEditing', 'insertImage',
                     'bulletedList', 'numberedList', 'todoList', '|',  'blockQuote', 'imageUpload', '|',
                     'fontSize', 'fontFamily', 'fontColor', 'fontBackgroundColor', 'mediaEmbed', 'removeFormat',
-                    'insertTable', 'lineHeight'
+                    'insertTable', 'lineHeight', 'alignment'
                     ],
             'shouldNotGroupWhenFull': True
         },
